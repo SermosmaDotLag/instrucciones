2f06bf58bec5f11631568e0d579a9e8f7f9c9c21
Sergio Mosquera Marin <smosqueramarin@gmail.com>
2023-10-04 12:20:38 +0200

    Primera subida de instrucciones mientras documento

---
+++ comandos.bat
@@ -0,0 +1,10 @@
+//Create a GitHub repo to clone to, usually it takes the same name as the directory we are pushing
+
+cd <relative path for the repo>
+
+git init //initialize the repo
+
+git clone <url of the repo> //link the repo
+
+git add <files separate by coma> //upload the files to the staging area for the commit (if you want to upload all files put a single dot [git add .])
+
